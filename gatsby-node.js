532ea44091708c22eda499c4828b9ebb7da573e0
mayed <mayedabbas@yahoo.com>
2019-10-16 13:57:11 +0400

    append description of the add command

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,12 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage } = actions
+  // Make the front page match everything client side.
+  // Normally your paths should be a bit more judicious.
+  if (page.path === `/`) {
+    page.matchPath = `/*`
+    createPage(page)
+  }
+}
 
-// You can delete this file if you're not using it
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
-import { Router, Link } from "@reach/router"
+import { Router, Link, Location } from "@reach/router"
+import { TransitionGroup, CSSTransition } from "react-transition-group"
+import "./main.css"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const App = () => (
+  <div className="app">
+    <nav className="nav">
+      <Link to="/">Page 1</Link> <Link to="page/2">Page 2</Link>
+      {` `}
+      <Link to="page/3">Page 3</Link> <Link to="page/4">Page 4</Link>
+    </nav>
 
-let Home = () => <div>Home</div>
-let Dash = () => <div>Dash</div>
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Router>
-    <Home path="/" />
-    <Dash path="dashboard" />
-  </Router>
-  </Layout>
+    <FadeTransitionRouter>
+      <Page path="/" page="1" />
+      <Page path="page/:page" />
+    </FadeTransitionRouter>
+  </div>
 )
 
-export default IndexPage
+const FadeTransitionRouter = props => (
+  <Location>
+    {({ location }) => (
+      <TransitionGroup className="transition-group">
+        <CSSTransition key={location.key} classNames="fade" timeout={500}>
+          {/* the only difference between a router animation and
+              any other animation is that you have to pass the
+              location to the router so the old screen renders
+              the "old location" */}
+          <Router location={location} className="router">
+            {props.children}
+          </Router>
+        </CSSTransition>
+      </TransitionGroup>
+    )}
+  </Location>
+)
+
+const Page = props => (
+  <div
+    className="page"
+    style={{ background: `hsl(${props.page * 75}, 60%, 60%)` }}
+  >
+    {props.page}
+  </div>
+)
+
+export default App
