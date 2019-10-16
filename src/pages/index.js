9c6e1de0f0fe0a60f27f4492798d2ced7a66d869
mayed <55652148+mayedalmaazmi@users.noreply.github.com>
2019-10-16 13:33:20 +0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Router, Link } from "@reach/router"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+let Home = () => <div>Home</div>
+let Dash = () => <div>Dash</div>
 
 const IndexPage = () => (
   <Layout>
@@ -14,7 +18,10 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Router>
+    <Home path="/" />
+    <Dash path="dashboard" />
+  </Router>
   </Layout>
 )
 
